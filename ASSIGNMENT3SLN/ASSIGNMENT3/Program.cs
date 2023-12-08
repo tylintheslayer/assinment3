b4bd9c00eeb357b663cac36ab4a2cad47548c013
tylintheslayer <151826095+tylintheslayer@users.noreply.github.com>
2023-12-08 17:01:59 +0200

    first commit

---
+++ ASSIGNMENT3SLN/ASSIGNMENT3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ASSIGNMENT3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transportofchoice Person1 = new Transportofchoice("taxi", 5, true);
+            Console.WriteLine($"It is {Person1.TravelingToWork} that Person 1 travels to work using a {Person1.FormOfTransport} and travels {Person1.FrequencyOfTravel} times a week.");
+        }
+    }
+}
+
---
+++ ASSIGNMENT3SLN/ASSIGNMENT3/Transportofchoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT3
+{
+    internal class Transportofchoice
+    {
+        public string FormOfTransport;
+        public int FrequencyOfTravel;
+        public bool TravelingToWork;
+        
+        public Transportofchoice(string formoftransport, int frequencyoftravel, bool travelingToWork)
+        {
+            FormOfTransport = formoftransport;  
+            FrequencyOfTravel = frequencyoftravel;
+            TravelingToWork = travelingToWork;
+        }
+
+        public string FormOfTransportation()
+        {
+            return FormOfTransport;
+        }
+        public bool WorkTravel()
+        {
+            return TravelingToWork = !TravelingToWork;
+
+        }
+
+
+
+
+
+    } 
+}
